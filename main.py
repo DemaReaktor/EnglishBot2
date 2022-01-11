188f24f2de3766f6370d17bd348889cb72c34785
Ira Omelchenko <81821850+IraOmel@users.noreply.github.com>
2022-01-11 21:00:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from Classes.bot import *
+from telebot import TeleBot
+from dispatcher import dp
+import handlers
+from handlers.personal_actions import bot
 
 if __name__ == "__main__":
     bot.polling(none_stop=True, interval=0)
